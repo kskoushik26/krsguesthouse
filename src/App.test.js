59b21ea440e4cc7aa77c948d6c3f95c3e35bdf42
Koushik KS <44638962+kskoushik26@users.noreply.github.com>
2026-09-17 22:21:18 +0530

    SEO optimization

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the guest house home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(
+    screen.getByRole('heading', {
+      name: /a comfortable stay, near to the siganduru chowdeshwari temple/i,
+    })
+  ).toBeInTheDocument();
 });
